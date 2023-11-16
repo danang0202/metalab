7ffc056dbf973e69f8b2c9d28cd6eba85ec1535e
Danang Wisnu Prabowo <222111975@stis.ac.id>
2023-11-17 02:57:31 +0700

    uplaodd

--- job-hiring/job-hiring/routes/web.php
+++ job-hiring/job-hiring/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\JobController;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,7 @@ use App\Http\Controllers\JobController;
 
 
 Route::get('/test', function(){
-    return view('emailNotif');
+    Artisan::call('storage:link');
 });
 
 // // Route untuk client start
